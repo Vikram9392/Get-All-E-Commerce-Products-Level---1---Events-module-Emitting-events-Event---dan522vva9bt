fef2e9f6c485829277dfedc0268fcb055628d2a0
Vikram9392 <vikramvallala12@gmail.com>
2024-01-30 17:46:57 +0530

    done

--- src/app.js
+++ src/app.js
@@ -4,11 +4,19 @@ const app = express();
 
 // Importing products from products.json file
 const products = JSON.parse(fs.readFileSync(`${__dirname}/data/products.json`));
+console.log(products)
 
 // Middlewares
 app.use(express.json());
 
 // Write GET endpoint for sending all the products to client here
 // Endpoint - /api/v1/products
+app.get("/api/v1/products",(req,res)=>{
+    res.status(200).json({
+        status: "success", 
+      message: "Product fetched successfully",
+      data: products
+    })
+})
 
 module.exports = app;
