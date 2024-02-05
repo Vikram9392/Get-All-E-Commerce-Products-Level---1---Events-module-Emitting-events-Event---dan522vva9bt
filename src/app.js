051ab407da3b3dc7001a75eb3618c21a80e0ab3f
Vikram9392 <vikramvallala12@gmail.com>
2024-02-05 18:26:33 +0530

    done

--- src/app.js
+++ src/app.js
@@ -11,6 +11,19 @@ app.use(express.json());
 
 // Write GET endpoint for sending all the products to client here
 // Endpoint - /api/v1/products
+app.post('/api/v1/products',(req,res)=>{
+  const{name,price,quantity}=req.body;
+  const newId=products[products.length-1].id+1;
+  const newProduct={name,price,quantity,id:newId}
+  products.push(newProduct);
+  fs.writeFile(`${__dirname}/data/product.json`,JSON.stringify(products),err=>{
+    res.status(201).json({
+      status:"Success",
+      message:"Product added successfully",
+      data:{newProduct}
+    })
+  })
+})
 app.get("/api/v1/products",(req,res)=>{
     res.status(200).json({
         status: "success", 
